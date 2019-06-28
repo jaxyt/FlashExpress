5324c0dc165b7d3ec0724e6fe68c7cbfec467e00
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-06-28 13:59:08 -0400

    learned some pug logic

--- app.js
+++ app.js
@@ -2,14 +2,37 @@ const express = require('express');
 
 const app = express();
 
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+  ];
+
+const friends = [
+    {first: 'blake', last: 'ross'},
+    {first: 'nick', last: 'klein'},
+    {first: 'mark', last: 'wickard'},
+    {first: 'casey', last: 'schneider'},
+    {first: 'sean', last: 'smalley'}
+]
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/cards', (req, res) => {
+    res.render('card', { prompt: `Who is buried in Grant's tomb?` });
+});
+
+
 app.get('/hello', (req, res) => {
-    res.render('hello');
+    //res.locals.prompt = `Who is buried in Grant's tomb?`;
+    res.render('hello', { prompt: `Who is buried in Grant's tomb?`,  colors, friends });
 });
 
 app.listen(3000, ()=>{
